ccf6b937e3f30daa07c4a6dbb75135f88b96a09d
David Calixto <davidcalixto9@gmail.com>
2019-11-04 11:41:40 -0500

    Adding

---
+++ haunted-classroom/Assets/Scripts/SpawnDeskAndChair.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDeskAndChair : MonoBehaviour
+{
+    public GameObject DeskAndChairprefab;
+    GameObject DeskAndChair;
+    void Start()
+    {
+       DeskAndChair = Instantiate(DeskAndChairprefab, new Vector3(transform.position.x,transform.position.y,transform.position.z), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       DeskAndChair.transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time, 2), transform.position.z);
+    }
+}
